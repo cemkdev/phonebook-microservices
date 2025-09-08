4ac9cb38fa94760dc872c2b2d9bf1580359868a7
cemkdev <79233363+cemkdev@users.noreply.github.com>
2025-09-08 23:17:40 +0300

    feat(shared): add ContactInfoAdded/Removed + ContactDeleted events

---
+++ Shared/Shared.Contracts/ContactEvents/ContactDeletedEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Contracts.ContactEvents
+{
+    public record class ContactDeletedEvent
+    {
+        public Guid ContactId { get; set; }
+        public DateTime OccurredAt { get; set; }
+    }
+}
---
+++ Shared/Shared.Contracts/ContactInfoEvents/ContactInfoAddedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Contracts.ContactInfoEvents
+{
+    public record class ContactInfoAddedEvent
+    {
+        public Guid ContactId { get; init; }
+        public short InfoType { get; init; } // enum
+        public string Content { get; init; }
+        public DateTime OccurredAt { get; init; }
+    }
+}
