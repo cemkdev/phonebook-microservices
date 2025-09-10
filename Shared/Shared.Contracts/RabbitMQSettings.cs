bc89ca2cacd0c967b721c724cd6a5a8f344f629e
cemkdev <79233363+cemkdev@users.noreply.github.com>
2025-09-10 16:25:50 +0300

    feat(shared): add ReportRequested event and queue name

--- Shared/Shared.Contracts/RabbitMQSettings.cs
+++ Shared/Shared.Contracts/RabbitMQSettings.cs
@@ -5,5 +5,6 @@
         public const string Worker_ContactInfoAddedEventQueue = "worker-contact-info-added-event-queue";
         public const string Worker_ContactInfoRemovedEventQueue = "worker-contact-info-removed-event-queue";
         public const string Worker_ContactDeletedEventQueue = "worker-contact-deleted-event-queue";
+        public const string Worker_ReportRequestedEventQueue = "worker-report-requested-event-queue";
     }
 }
---
+++ Shared/Shared.Contracts/ReportEvents/ReportRequestedEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.Contracts.ReportEvents
+{
+    public record class ReportRequestedEvent
+    {
+        public Guid ReportId { get; init; }
+    }
+}
